b62da16b639ef5697fd02ec72f5d3ff42aa284ac
Aron Bergman <bathingrad@gmail.com>
2018-09-23 16:28:33 +0200

    Add relevant functions to the main function

--- main.c
+++ main.c
@@ -1,7 +1,15 @@
-#include <stdio.h>
-#include "draw.h"
+#include <time.h>
+#include <stdlib.h>
+#include "snake.h"
+#include "board.h"
 
 int main(int argc, char **argv)
 {
+	srand(time(NULL));
+	int width = 5;
+	int height = 5;
+	snake snake = snake_init(width, height);
+	board board = board_init(width, height, snake);
+	board_print(board);
 	return 0;
 }
