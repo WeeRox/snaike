920249e1fb5527f70ffe1b45417f5d18028ec3a5
Aron Bergman <bathingrad@gmail.com>
2018-09-26 14:07:50 +0200

    Create basic game loop

--- main.c
+++ main.c
@@ -8,11 +8,28 @@ int main(int argc, char **argv)
 {
 	srand(time(NULL));
 	atexit(draw_reset);
+	struct timespec sleep;
+
+	sleep.tv_sec = 0;
+	sleep.tv_nsec = 900000000;
 
 	int width = 5;
 	int height = 5;
 	snake *snake = snake_init(width, height);
 	board board = board_init(width, height, snake);
+
 	board_print(board);
+
+	while (1)
+	{
+		snake_move(snake);
+		if (snake_collision(snake) || board_collision(board))
+		{
+			return 1;
+		}
+		board_print(board);
+		nanosleep(&sleep, NULL);
+	}
+
 	return 0;
 }
