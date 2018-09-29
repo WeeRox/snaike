39c6d6341e6f4656021aa4ad4f8af9a539666afa
Aron Bergman <bathingrad@gmail.com>
2018-09-29 20:58:09 +0200

    Add function to grow snake

--- snake.c
+++ snake.c
@@ -14,6 +14,9 @@ snake *snake_init(int w, int h)
 	snake->snake[0][1] = rand() % h;
 
 	snake->direction = rand() % 4;
+	snake->pos_grow = malloc(sizeof(int) * 2);
+	snake->pos_grow[0] = snake->snake[0][0];
+	snake->pos_grow[1] = snake->snake[0][1];
 
 	return snake;
 }
@@ -43,9 +46,23 @@ void snake_move(snake *snake)
 			break;
 	}
 
+	snake->pos_grow[0] = snake->snake[snake->length - 1][0];
+	snake->pos_grow[1] = snake->snake[snake->length - 1][1];
+
 	memcpy(&new[1], snake->snake, sizeof(int *) * (snake->length - 1));
 
 	snake->snake = new;
+}
+
+void snake_grow(snake *snake)
+{
+	snake->length++;
+
+	snake->snake = realloc(snake->snake, sizeof(int *) * snake->length);
+	snake->snake[snake->length - 1] = malloc(sizeof(int) * 2);
+
+	snake->snake[snake->length - 1][0] = snake->pos_grow[0];
+	snake->snake[snake->length - 1][1] = snake->pos_grow[1];
 }
 
 int snake_collision(snake *snake)
--- snake.h
+++ snake.h
@@ -14,10 +14,12 @@ typedef struct snake
 	int length;
 	int **snake;
 	direction direction;
+	int *pos_grow;
 } snake;
 
 snake *snake_init(int w, int h);
 void snake_move(snake *snake);
+void snake_grow(snake *snake);
 int snake_collision(snake *snake);
 
 #endif /* __SNAIKE_SNAKE_H */
