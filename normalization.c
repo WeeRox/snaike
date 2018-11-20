b44dfede2ab53912aa173d42c122dc6e2eb26969
Aron Bergman <bathingrad@gmail.com>
2018-11-20 19:41:27 +0100

    Add minmax normalization function

---
+++ normalization.c
@@ -0,0 +1,4 @@
+int minmax(int x, int min, int max)
+{
+	return ((x-min)/(max-min));
+}
---
+++ normalization.h
@@ -0,0 +1,6 @@
+#ifndef __SNAIKE_NORMALIZATION_H
+#define __SNAIKE_NORMALIZATION_H
+
+int minmax(int x, int min, int max);
+
+#endif /* __SNAIKE_NORMALIZATION_H */
