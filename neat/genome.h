3cc145109527ce3ea308a1fd77fb837581016e3d
Aron Bergman <bathingrad@gmail.com>
2018-12-05 15:13:49 +0100

    Create genome datastructure

--- neat/genome.h
+++ neat/genome.h
@@ -1,16 +1,18 @@
 #ifndef __SNAIKE_NEAT_GENOME_H
 #define __SNAIKE_NEAT_GENOME_H
 
+#include <hashmap.h>
+
 enum type {
 	INPUT,
 	OUTPUT,
 	HIDDEN
-}
+};
 
 struct node_gene {
 	unsigned int id;
 	enum type type;
-}
+};
 
 struct connection_gene {
 	unsigned int id; // also known as innovation number
@@ -18,6 +20,11 @@ struct connection_gene {
 	struct node_gene out;
 	float weight;
 	int enabled;
-}
+};
+
+struct genome {
+	struct hashmap *connection_genes;
+	struct hashmap *node_genes;
+};
 
 #endif /* __SNAIKE_NEAT_GENOME_H */
