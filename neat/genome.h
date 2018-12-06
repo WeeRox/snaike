c6081fe4b7777782ccad1f177769702af40bf76b
Aron Bergman <bathingrad@gmail.com>
2018-12-06 19:58:10 +0100

    Change in and out nodes to pointers

--- neat/genome.h
+++ neat/genome.h
@@ -18,8 +18,8 @@ struct node_gene {
 
 struct connection_gene {
 	unsigned int id; // also known as innovation number
-	struct node_gene in;
-	struct node_gene out;
+	struct node_gene *in;
+	struct node_gene *out;
 	float weight;
 	int enabled;
 };
