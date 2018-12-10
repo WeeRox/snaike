9a4d8d698eb57deaad1550bc9b1b3922625ccde4
Aron Bergman <bathingrad@gmail.com>
2018-12-10 11:02:57 +0100

    Add neat to bin_PROGRAMS

---
+++ neat/neat.c
@@ -0,0 +1,18 @@
+#include "board.h"
+
+unsigned int innovation_number = 0;
+
+void setup(void)
+{
+	return;
+}
+
+void teardown(void)
+{
+	return;
+}
+
+direction get_direction(board board)
+{
+	return NORTH;
+}
